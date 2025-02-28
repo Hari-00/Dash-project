9f2d1c8fee0b12695012ee80184a05fa21e3e17f
Hari krishna <111626929+Hari-00@users.noreply.github.com>
2025-02-28 16:07:44 +0530

    Update app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import glob
+
+data_folder = "data/*.csv"
+all_files=glob.glob(data_folder)
+for file in all_files:
+    df=pd.read_csv(file)
+    df['price'] = df['price'].str.replace('$', '', regex=False).astype(float)
+    ch=df[df['product'] == 'pink morsel']
+    ch['sales']=ch['price']*ch['quantity']
+    ch.drop(['price','quantity'],axis=1, inplace=True)
+    ch=ch[['product','sales','date','region']]
+    print(ch)
